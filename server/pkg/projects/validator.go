e05d8ce5f20cac3a39b4793218de1e380d1037c0
Ali Masyhur <alimasyhur@student.uns.ac.id>
2017-10-18 11:31:34 +0700

    added missing comma ,

--- server/pkg/projects/validator.go
+++ server/pkg/projects/validator.go
@@ -2,14 +2,12 @@ package projects
 
 import (
 	"github.com/darwinfroese/hacksite/server/models"
-
-	"github.com/go-ozzo/ozzo-validation"
+	validation "src/github.com/go-ozzo/ozzo-validation"
 )
 
 // ValidateProject checks if the model is valid
-
 func (project models.Project) ValidateProject() error {
 	return validation.ValidateStruct(&project,
-		validation.Field(&project.Name, validation.Required)
+		validation.Field(&project.Name, validation.Required),
 	)
 }
